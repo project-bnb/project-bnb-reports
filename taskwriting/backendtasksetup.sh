92ab69b5588a7f03d363d7bc4591a0a8501f56f5
MindfulLearner <dimajosh1009@gmail.com>
2024-11-23 00:44:39 +0100

    updated script

--- taskwriting/backendtasksetup.sh
+++ taskwriting/backendtasksetup.sh
@@ -5,6 +5,22 @@ REPO="laravel-db-bnb"
 
 create_issue() {
 
+  local title=$1
+  local body=$2
+
+  title="Creare filtraggi API | Backend"
+  body=$(cat <<EOF
+**Descrizione:**  
+Creare i filtraggi per le API, che dovranno includere i seguenti filtri:
+- Numero di stanze
+- Letti
+- Bagni
+- Metri quadrati
+- Prezzo
+EOF
+  )
+  gh issue create --repo "$ORGANIZATION/$REPO" --title "$title" --body "$body"
+
 }
 
 while true; do
